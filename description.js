23d60cb12e6bd46d8958bb35ab1c10ec6a9fc4ce
Nbregvadzzze <129889614+Nbregvadzzze@users.noreply.github.com>
2023-06-06 19:37:13 +0400

    update

--- description.js
+++ description.js
@@ -5,6 +5,13 @@ console.log(queryString);
 const urlParams = new URLSearchParams(queryString);
 const id = urlParams.get("id");
 
+const getPokemonsCharacteristic = async (id) =>{
+  const url = `https://pokeapi.co/api/v2/characteristic/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  return data
+}
+
 const getPokemons = async (id) => {
   console.log(id);
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
@@ -15,26 +22,50 @@ const getPokemons = async (id) => {
 };
 getPokemons(id);
 
-const createPokemonCard = (pokemon) => {
+const createPokemonCard = (pokemon, characteristic) => {
   const pokemonEl = document.createElement("div");
+  const descriptions = characteristic.descriptions;
+  const { description } = descriptions.find((desc)) => {
+    return desc.language.name === "en";
+  }
 
   pokemonEl.classList.add("pokemon");
   console.log(pokemon.name);
 
   const pokemonInnerHtml = `
-        <div class="img-container">
+        <div class="pokemonDescription">
+          <div class="pokemon-image">
             <img src="${pokemon.sprites.front_default}"> 
         </div>
-       
-        <div class = "info">
-                <h3>
-                    ${pokemon.name}
-                </h3>
-                <small>
-                    
-                </small>
+        <div class = "pokemon-info">
+                <h3>${pokemon.name}</h3>
+                <p>${description}</p>
+        </div>
+        <ul class="pokemon-stats">
+        ${pokemon.stats.map((stat) =>{
+          return`
+          <li>${stat.stat.name}: ${stat.base_stat}</li>`
+        })}"
+        .join("")}
+        ></ul>
+        <div class="pokemon-height">
+        <p>Height: ${pokemon.height}</p>
+        <p>Weight: ${pokemon.weight}</p>
+        </div>
+        <div class="moves">
+        <h3>Pokemon moves</h3>
+        <ul class="pokemon-moves">
+            ${pokemon.moves
+            .map((move) => {
+              return `
+              <li>${move.move.name}</li>`
+            })
+          .join("")}
+        </ul>
+        </div>
         </div>
   `;
+       
   pokemonEl.addEventListener("click", () => {
     window.location.href = `description.html?name=${pokemon.name}&id=${pokemon.id}`;
   });
